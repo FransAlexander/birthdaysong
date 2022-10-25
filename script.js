ae75ebd39ef654885da8aba0012688b013749438
Frans Alexander <dev.norberg@gmail.com>
2022-10-25 16:04:17 +0200

    DOM manipulation add form action depending on date enterd

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-let subBtn = document.querySelector(".sub_btn")
+
+
+const container = document.querySelector(".container_center")
+const newForm = document.createElement("form")
+const newInput = document.createElement("input")
+const newButton = document.createElement("button")
+
+
+newForm.classList.add("date_from")
+newInput.classList.add("input")
+newButton.classList.add("sub_btn")
+newInput.setAttribute("placeholder","yy/mm/dd")
+newInput.setAttribute("type","number")
+newForm.setAttribute("type","submit")
+
+newForm.appendChild(newInput)
+newForm.appendChild(newButton)
+container.appendChild(newForm)
+
 let input = document.querySelector(".input")
 let form = document.querySelector(".date_form")
-let linkDiv = document.querySelector(".linkDiv")
- //yyyy/dd/mm
- 
-input.addEventListener("input", ()=>{
-  
-  let inputVal = parseInt(input.value)
-  
-  if(inputVal >= 590101 && inputVal <= 590118){
-    let newLink = document.createElement('a')
-    newLink.classList.add("newLink")
-    newLink.setAttribute("href","1959/59jan1-18.html")
-    newLink.textContent = "click me"
-    linkDiv.appendChild(newLink)
-  }
+let btn = document.querySelector(".sub_btn")
 
+btn.addEventListener("click",()=>{
+  let inputVal = parseInt(input.value)
+  if(inputVal>=590101 && inputVal<=590118){
+    newForm.setAttribute("action","1959/59jan1-18.html")
+  }else if(inputVal>=590119 && inputVal<=590208){
+    newForm.setAttribute("action","1959/59jan19-feb8.html")
+  }
 })
 
 
-function getYear(){
-  let inputVal = parseInt(input.value)
-  console.log(inputVal)
-}
+
